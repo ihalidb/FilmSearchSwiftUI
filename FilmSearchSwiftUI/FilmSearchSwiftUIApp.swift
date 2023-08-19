12e84bfc6bb2f5ba9509302c93e998a6074d24a5
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-08-19 10:07:41 +0300

    Initial Commit

---
+++ FilmSearchSwiftUI/FilmSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmSearchSwiftUIApp.swift
+//  FilmSearchSwiftUI
+//
+//  Created by İbrahim Halid Bayrak on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FilmSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
